118df185a8422dcb7fcc0f7897b3b46df9837d17
erica <erikaisa7dev@gmail.com>
2022-04-02 22:12:00 -0300

    teste

---
+++ index.py
@@ -0,0 +1,13 @@
+
+def bolo ():
+    print("E aí maluco!")
+
+bolo()
+
+def beijos (gato1, gato=1, gato2 ):
+    print("Eu pego "+ gata1 + gata2)
+
+beijos("Gabi", "Vivi")
+
+def ground(name):
+    print(f'hey {ground}')
